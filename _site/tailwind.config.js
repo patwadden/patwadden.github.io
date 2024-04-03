fd038466ffbcb4f3521b5aeb8e7cac9a65c86b89
Patrick Wadden <patwadden@gmail.com>
2024-04-03 18:02:09 +0100

    New site

---
+++ _site/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./_works/*.html", "./_layouts/*.html"],
+  theme: {
+    fontFamily: {
+      serif: ['Lato', 'ui-sans-serif', 'system-ui', 'sans-serif', "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: ["./*.html", "./_works/*.html", "./_layouts/*.html"],
   theme: {
     fontFamily: {
       serif: ['Lato', 'ui-sans-serif', 'system-ui', 'sans-serif', "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
