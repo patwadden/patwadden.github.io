9f5048776d55fe5ce490ccd180b57436e3a909d4
Patrick Wadden <patwadden@gmail.com>
2024-04-03 16:01:47 +0100

    Version to WIP

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    fontFamily: {
+      serif: ['Lato', 'ui-sans-serif', 'system-ui', 'sans-serif', "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
